3d89b75de2930624d9dbbf054aadadf21d32e3ab
husk <huskgit@gmail.com>
2022-12-27 16:41:32 +0900

    lecture : 40. Animation Blueprint

--- Blaster/Source/Blaster/Character/BlasterCharacter.cpp
+++ Blaster/Source/Blaster/Character/BlasterCharacter.cpp
@@ -4,6 +4,7 @@
 #include "BlasterCharacter.h"
 
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 ABlasterCharacter::ABlasterCharacter()
@@ -19,6 +20,9 @@ ABlasterCharacter::ABlasterCharacter()
 
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 void ABlasterCharacter::BeginPlay()
